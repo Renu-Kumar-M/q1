b95ed203f54d7e91053ae4b414be7587a2034727
sireeshagarlapati29 <sireeshagarlapati6@gmail.com>
2026-09-02 16:23:12 +0530

    Initial commit with app and Jenkinsfile

---
+++ sum.py
@@ -0,0 +1,11 @@
+#NAME: RENU KUMAR. M
+# REG NO: 24MIS0227
+# app.py
+print("--- Question 1: Basic Python Application ---")
+num1 = 40
+num2 = 60
+total = num1 + num2
+print("Number 1:", num1)
+print("Number 2:", num2)
+print("Total Sum:", total)
+
